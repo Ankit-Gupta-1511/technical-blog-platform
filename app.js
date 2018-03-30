7b11378354d265b8e80e693ea0b8df00cf32030a
Ankit-Gupta-1511 <ankit.gupta.cs1511@gmail.com>
2018-03-30 13:05:02 +0530

    google oauth

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+/////////////////////////
+// passport config
 
+require('./config/passport')(passport);
+
+/////////////////////////////////
+
+// Load Routes modules
+const auth = require('./routes/auth');
+
+
+//////////////////////////////////
+
+// creating routes
 
 app.get('/', (req, res) => {
     res.send('It works');
 });
 
+// using routes
+app.use('/auth', auth);
+
+
+///////////////////////////////////
 
 app.listen(port, () => {
         console.log(`Server started on port : ${port}`);
