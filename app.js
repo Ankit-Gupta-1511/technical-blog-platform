3e9d50ed9a30ddf15455856779eb8d77348e09cd
Ankit-Gupta-1511 <ankit.gupta.cs1511@gmail.com>
2018-03-30 11:52:59 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const port = process.env.PORT | 5000;
+
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('It works');
+});
+
+
+app.listen(port, () =>{
+        console.log(`Server started on port : ${port}`);
+});
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientId: '757755538309-smqqe7sr2bc4n0i1h5bgnfe3nv7j1q1m.apps.googleusercontent.com',
+    googleClientSecret: 'ayNVBY5z7gpEAt1-5y8nURWm'
+}
