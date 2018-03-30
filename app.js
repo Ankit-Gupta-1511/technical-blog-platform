b37b42ec1aad1d1b0d83d2c7ac6cd196cbb43bfd
Ankit-Gupta-1511 <ankit.gupta.cs1511@gmail.com>
2018-03-30 12:23:00 +0530

    deployment config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(port, () =>{
+app.listen(port, () => {
         console.log(`Server started on port : ${port}`);
 });
